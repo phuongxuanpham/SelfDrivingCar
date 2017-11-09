f9135cd0c56c642296547ef9a717249ee011daaa
Phuong Pham <phuongxuanpham@yahoo.com>
2017-11-09 01:26:43 -0600

    Update Project 5 - Add code files

---
+++ CarND-Vehicle-Detection-Project5/codes/draw_bboxes.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 17:15:42 2017
+
+@author: phuongpham
+"""
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+image = mpimg.imread('bbox-example-image.jpg')
+
+# Define a function that takes an image, a list of bounding boxes, 
+# and optional color tuple and line thickness as inputs
+# then draws boxes in that color on the output
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # make a copy of the image
+    draw_img = np.copy(img)
+    # draw each bounding box on your image copy using cv2.rectangle()
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
+    # return the image copy with boxes drawn
+    return draw_img # Change this line to return image copy with boxes
+# Add bounding boxes in this format, these are just example coordinates.
+bboxes = [((100, 100), (200, 200)), ((300, 300), (400, 400))]
+
+result = draw_boxes(image, bboxes)
+plt.imshow(result)
+
+# For displaying image from console
+from PIL import Image                                                                                
+img = Image.fromarray(result)
+img.show() 
---
+++ CarND-Vehicle-Detection-Project5/codes/vehicle_detection.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 00:07:28 2017
+
+@author: phuongpham
+"""
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+from skimage.feature import hog
+import lesson_functions
+
+def pipeline():
+    
